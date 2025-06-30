d48fe3495f606e850db8aec5510072ac01e44413
yuvarajm20 <YUVARAJMASILAMANI2000@GMAIL.COM>
2025-06-30 18:46:26 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CodeCoverageAnalyzer.Model;
+using CodeCoverageAnalyzer.ViewModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodeCoverageAnalyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var vm = (MainViewModel)DataContext;
+
+            if (e.NewValue is CoverageNode node && node.File != null)
+            {
+                vm.SelectedFile = node.File;
+
+                var converter = new CoverageFileToDocumentConverter();
+                CodeViewer.Document = (FlowDocument)converter.Convert(node.File, typeof(FlowDocument), null, null);
+            }
+            else
+            {
+                vm.SelectedFile = null;
+                CodeViewer.Document = new FlowDocument(new Paragraph(new Run(string.Empty))); // Clear
+            }
+        }
+    }
+}
